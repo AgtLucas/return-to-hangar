6cac3c03318c8639e8363da10469bd67b40e6926
Lucas <lucas@agtlucas.com>
2016-04-02 17:28:31 -0300

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import fs from 'fs'
 
 let output = fs.readFileSync('data.txt', 'utf8')
+  .trim()
   .split('\n')
+  .map(line => line.split('\t'))
+  // .reduce((customers, line) => {
+  //
+  // }, {})
 
 console.log('output', output)
